a5872a20d6bbbf53ef7a88d0d5103c6dde76e5a8
Mayank Aggarwal <56104618+mayankAgg06@users.noreply.github.com>
2025-09-25 11:50:16 +0530

    added controller

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,10 +1,26 @@
 import mongoose from "mongoose";
 
 const bookSchema = mongoose.Schema({
-    title:{type: String, required:[true,'Title is required']},
-    author:{type: String, required:[true,'Author is required']},
-    genre:{type: String, required:[true,'Genre is required']},
-    publishedDate:{type: Date, required:[true,'Publishing Date is required']}
+    title:{
+        type: String, 
+        required:[true,'Title is required'],
+        minlength:[3,'The title cannot be shorter than 3 characters']
+    },
+    author:{
+        type: String, 
+        required:[true,'Author is required'],
+        minlength:[3,'Author Name cannot be shorter than 3 characters']
+    },
+    genre:{
+        type: String, 
+        required:[true,'Genre is required'],
+    },
+    publishedDate:{
+        type: Date, 
+        required:[true,'Publishing Date is required'],
+        min:[1900,'Published year cannot be earlier than 1900'],
+        max:[new Date().getFullYear(), 'Published date cannot be in future']
+    }
 })
 
 const Book = mongoose.model('Book',bookSchema);
